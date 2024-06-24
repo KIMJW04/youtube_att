0af0380d8802ee271d763d34b9ab3ca2d59db529
JinWoo Kim <kjw040416@gmail.com>
2024-06-25 05:17:39 +0900

    2024.06.24

--- src/components/video/VideoSlider.jsx
+++ src/components/video/VideoSlider.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import { Navigation, A11y, Autoplay, Mousewheel } from 'swiper/modules';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Navigation, A11y, Autoplay, Mousewheel } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Link } from "react-router-dom";
 
-import 'swiper/css';
-import 'swiper/css/navigation';
+import "swiper/css";
+import "swiper/css/navigation";
 
 const VideoSlider = ({ videos, id, title }) => {
     if (!videos || videos.length === 0) {
@@ -12,7 +12,7 @@ const VideoSlider = ({ videos, id, title }) => {
     }
 
     return (
-        <section id={id} className='slider__wrap'>
+        <section id={id} className="slider__wrap">
             <h2>{title}</h2>
             <Swiper
                 modules={[Navigation, A11y, Autoplay, Mousewheel]}
@@ -21,34 +21,31 @@ const VideoSlider = ({ videos, id, title }) => {
                 navigation
                 loop={true}
                 autoplay={{ delay: 3000, disableOnInteraction: false }}
-                mousewheel={true}
+                // mousewheel={{ sensitivity: 3 }}
                 speed={500} // 슬라이드 전환 속도를 줄여 매끄럽게
                 onSwiper={(swiper) => console.log(swiper)}
-                onSlideChange={() => console.log('slide change')}
+                onSlideChange={() => console.log("slide change")}
             >
                 {videos.map((video, index) => (
-                    <SwiperSlide key={index} className='video__slider'>
-                        <div className='video' key={index} >
+                    <SwiperSlide key={index} className="video__slider">
+                        <div className="video" key={index}>
                             <div className="video__thumb play__icon">
-                                <Link
-                                    to={`/video/${video.id.videoId}`}
-                                    style={{ backgroundImage: `url(${video.snippet.thumbnails.high.url})` }}>
-                                </Link>
+                                <Link to={`/video/${video.id.videoId}`} style={{ backgroundImage: `url(${video.snippet.thumbnails.high.url})` }}></Link>
                             </div>
                             <div className="video__info">
-                                <div className='title'>
+                                <div className="title">
                                     <Link to={`/video/${video.id.videoId}`}>{video.snippet.title}</Link>
                                 </div>
-                                <div className='author'>
+                                <div className="author">
                                     <Link to={`/channel/${video.snippet.channelId}`}>{video.snippet.channelTitle}</Link>
                                 </div>
                             </div>
-                        </div >
+                        </div>
                     </SwiperSlide>
                 ))}
             </Swiper>
         </section>
     );
-}
+};
 
 export default VideoSlider;
